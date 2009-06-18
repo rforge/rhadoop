7180e3a3a3f6520a1b719c40d14b0d7ccd4d229c
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-18 07:58:20 +0000

    Further HDFS Java API tests.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhadoop@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/hive/R/java.R
+++ pkg/hive/R/java.R
@@ -1,7 +1,12 @@
+.DFS_java <- function(...) {
+    core_default <- .jnew("org/apache/hadoop/fs/Path", file.path(hive:::hadoop_home(hive()), "src", "core", "core-default.xml"))
+    core_site <- .jnew("org/apache/hadoop/fs/Path", file.path(hive:::hadoop_home(hive()), "conf", "core-site.xml"))
 
-.DFS_java <- function(x){
     configuration <- .jnew("org/apache/hadoop/conf/Configuration")
-    configuration
+    .jcall(configuration, "V", "addResource", core_default)
+    .jcall(configuration, "V", "addResource", core_site)
+
+    .jcall(configuration, "I", "size")
+
     .jcall("org/apache/hadoop/fs/FileSystem", "Lorg/apache/hadoop/fs/FileSystem;", "get", configuration)
-    #.jcall(configuration, "S", "get", "hadoop.tmp.dir")
 }
--- pkg/hive/R/zzz.R
+++ pkg/hive/R/zzz.R
@@ -15,10 +15,10 @@ hive <- local({
     hive(.hinit())
     if( is.environment(hive()))
     {
-        config_dirs <- c(file.path(hadoop_home(hive()), "hadoop-0.20.0-core.jar"), file.path(hadoop_home(hive()), "lib", "commons-logging-1.0.4.jar"))
+        config_dirs <- c(file.path(hadoop_home(hive()), "hadoop-0.20.0-core.jar"),
+                         file.path(hadoop_home(hive()), "lib", "commons-logging-1.0.4.jar"))
         ## add Java support
-        require("rJava")
-        .jpackage(pkgname, morePaths = config_dirs)
+        .jpackage(pkgname, morePaths = config_dirs, lib.loc = libname)
     }
 }
 
