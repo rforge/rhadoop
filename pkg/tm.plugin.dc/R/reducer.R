f41b047c918865322f1c694a5f177794539e4c46
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-15 08:54:11 +0000

    Clean up.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhadoop@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tm.plugin.dc/R/reducer.R
+++ pkg/tm.plugin.dc/R/reducer.R
@@ -1,9 +1,3 @@
-# TODO: Kurt fragen, ob das Sinn macht!
-empty_tdm <- structure(list(i = integer(0), j = integer(0), v = integer(0), nrow = 0, ncol = 0,
-                            dimnames = list(Terms = character(0), Docs = character(0)),
-                            Weighting = c("W1", "W2")),
-                       class = c("TermDocumentMatrix", "simple_triplet_matrix"))
-
 # NOTE: Works only for term-document matrices (and NOT document-term matrices)
 c_TermDocumentMatrix <-function(m, doc) {
     tf <- termFreq(doc)
@@ -11,7 +5,7 @@ c_TermDocumentMatrix <-function(m, doc) {
     m$dimnames <- list(Terms = c(Terms(m), setdiff(names(tf), Terms(m))),
                        Docs = c(Docs(m), ID(doc)))
     m$nrow <- length(Terms(m))
-    m$ncol <- ncol(m) + 1
+    m$ncol <- ncol(m) + 1 # better use length(Docs(m)) for consistency?
 
     m$i <- c(m$i, which(Terms(m) %in% names(tf)))
     m$j <- c(m$j, rep(nDocs(m), length(tf)))
