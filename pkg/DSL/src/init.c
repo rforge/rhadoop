ba2a4adca3517b5f733098684e66584df58204dd
khornik <khornik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-01-14 17:30:03 +0000

    Improve registration.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhadoop@161 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DSL/src/init.c
+++ pkg/DSL/src/init.c
@@ -4,13 +4,13 @@
 
 SEXP _collector2(SEXP x, SEXP y);
 
-static const R_CMethodDef CEntries[] = {
+static const R_CallMethodDef CallEntries[] = {
     {"_collector2", (DL_FUNC) &_collector2, 2},
     {NULL, NULL, 0}
 };
 
 void R_init_DSL(DllInfo *dll)
 {
-    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
